73eb372e694e23822f753034a5f8aa8d1da1ccf1
Bryant Baus <bbaus@mail.umw.edu>
2019-02-07 21:53:05 -0500

    I.3 final commit (1)

---
+++ IntroToProgramming/PartialSum_Baus.py
@@ -0,0 +1,47 @@
+"""
+@author: bdbaus
+"""
+
+import numpy as np
+
+########################
+#EQUATION 1
+#N is total number of terms in the partial sum sequence
+
+def SumCalcOne(N):
+    sum1=0
+    ResultArray = np.empty([N+1])
+    for ii in range(1,N+1):
+        temp=(np.log((ii**4)+ii+3))/((np.sqrt(ii))+3)
+        #not using this method print("term",ii,": ",temp)
+        ResultArray[ii] = temp
+        sum1+=temp
+    print("The sum for the first ",N," terms is ",sum1,"\n")
+    print("The first 15 terms are ",ResultArray[:15],"\n")
+    #really would like this to print line by line for aesthetic purposes
+    print("The last 15 terms are ",ResultArray[-15:],"\n")
+    #the arrays are providing e+XX at the end, not ideal
+
+#########################
+    
+print('Here is the partial summation for the first 15 terms of the "S_n" sequence:\n')      
+SumCalcOne(15)
+ 
+#########################
+       
+def SumCalcTwo(N):
+    sum1=0
+    ResultArray = np.empty([N+1])
+    for ii in range(1,N+1):
+        temp=(np.exp(ii/100)/(ii**10))
+        #print("term",ii,": ",temp)
+        sum1+=temp
+    print("The sum for the first ",N," terms is ",sum1,"\n")
+    print("The first 15 terms are ",ResultArray[:15],"\n")
+    print("The last 15 terms are ",ResultArray[-15:],"\n")
+    
+#########################
+
+print('Here is the partial summation for the first 15 terms of the "T_n" sequence:\n')
+SumCalcTwo(15)
+
