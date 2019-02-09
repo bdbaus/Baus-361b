9a8b7f0cf32adf30830b1aaa714ce7b84a76c11a
Bryant Baus <bbaus@mail.umw.edu>
2019-02-09 16:38:13 -0500

    Added a summation of my own creation. Possibly final commit.

--- IntroToProgramming/PartialSum_Baus.py
+++ IntroToProgramming/PartialSum_Baus.py
@@ -17,10 +17,8 @@ def SumCalcOne(N):
         ResultArray[ii] = temp
         sum1+=temp
     print("The sum for the first ",N," terms is ",sum1,"\n")
-    print("The first 15 terms are ",ResultArray[:15],"\n")
-    #really would like this to print line by line for aesthetic purposes
-    print("The last 15 terms are ",ResultArray[-15:],"\n")
-    #the arrays are providing e+XX at the end, not ideal
+    print("The first 15 terms are ",*ResultArray[:15],sep="\n")
+    print("The last 15 terms are ",*ResultArray[-15:],sep="\n")
 
 #########################
     
@@ -37,11 +35,29 @@ def SumCalcTwo(N):
         #print("term",ii,": ",temp)
         sum1+=temp
     print("The sum for the first ",N," terms is ",sum1,"\n")
-    print("The first 15 terms are ",ResultArray[:15],"\n")
-    print("The last 15 terms are ",ResultArray[-15:],"\n")
+    print("The first 15 terms are ",*ResultArray[:15],sep="\n")
+    print("The last 15 terms are ",*ResultArray[-15:],sep="\n")
     
 #########################
 
 print('Here is the partial summation for the first 15 terms of the "T_n" sequence:\n')
 SumCalcTwo(15)
 
+#########################
+
+def SumCalcThree(N):
+    sum1=0
+    ResultArray = np.empty([N+1])
+    for ii in range(1,N+1):
+        temp=(ii**2)*(np.pi/6)
+        print("term",ii,": ",temp)
+        ResultArray[ii]=temp
+        sum1+=temp
+    print("The sum for the first ",N," terms is ",sum1,"\n")
+    print("\n The first 15 terms are ",*ResultArray[:15],sep="\n")
+    print("\n The last 15 terms are ",*ResultArray[-15:],sep="\n")
+    
+#########################
+
+print('Here is the partial summation for the first 15 terms of a sequence of my own creation:\n')
+SumCalcThree(15)
