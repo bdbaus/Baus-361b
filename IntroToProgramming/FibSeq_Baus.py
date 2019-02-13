29676ebe4a82aafe73661962072e3522f838e93b
Bryant Baus <bbaus@mail.umw.edu>
2019-02-13 12:37:20 -0500

    i.5 fixed cassinis identity final

--- IntroToProgramming/FibSeq_Baus.py
+++ IntroToProgramming/FibSeq_Baus.py
@@ -22,23 +22,14 @@ def Fib(F0, F1, N):
         fiblst=[F0,F1]
         for i in range(2,N+1):
             fiblst.append(fiblst[i-1]+fiblst[i-2])
-        return fiblst
-        print("All terms: ",fiblst)
+        #print("All terms: ",fiblst)
         print("Last 10 terms: ",fiblst[-10:])
+        return fiblst
+        
 
-    
-"""
-#UNCOMMENT TO PROVIDE TEST LISTS 
-  
-Fib(0,1,0)
-Fib(0,1,1)
-Fib(0,1,2)
-Fib(4,5,20)
-"""
 
 def cat(n,r):    
     sequence = Fib(0,1,10)
-    #this is a list of the first 10 terms to prove catalin's identity
     term1=(sequence[n]**2)-(sequence[n-r]*sequence[n+r])
     term2=((-1)**(n-r))*(sequence[r]**2)
     print(term1,"=",term2)
@@ -47,10 +38,19 @@ def cat(n,r):
 #PROOF OF CATALAN'S IDENTITY, works with all numbers that satisfy condition
     
 def cas(n):
-    sequence = Fib(0,1,10)
-    term1=(sequence[n-1])*(sequence[n+1])-sequence[n]**2
-    term2=(-1)**n
-    print(term1,"=",term2)
     
-cat(4,6)
-cas(7)
+    sequence = Fib(0,1,n)
+    caslst=[]
+    #this is a list of the last 10 terms to prove catalin's identity
+    for i in range(n):
+        item1=(sequence[i]**2-(sequence[i+1])*(sequence[i-1]))
+        caslst.append(item1)
+        #caslst.append(term1)
+        #print(term1,"=",term2)
+    print(caslst[-10:])
+  
+#Fib(0,1,10)
+#cat(6,4)
+cas(10)
+
+#BOTH HOLD TRUE SO LONG AS THE FIRST TERMS ARE 0,1
