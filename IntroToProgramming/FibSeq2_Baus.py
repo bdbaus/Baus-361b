39dede0ee6a310afe264c6230cdcd486afa3ab89
Bryant Baus <bbaus@mail.umw.edu>
2019-02-15 12:31:12 -0500

    no message

--- IntroToProgramming/FibSeq2_Baus.py
+++ IntroToProgramming/FibSeq2_Baus.py
@@ -4,8 +4,29 @@ I.6
 @author: bdbaus
 """
 
+def Fib(N):
+    fiblst=[]
+    if N==0:
+        fiblst=[]
+        print(fiblst)
+        return fiblst
+    elif N==1:
+        fiblst=[0]
+        print(fiblst)
+        return fiblst
+    elif N==2:
+        fiblst=[0,1]
+        print(fiblst)
+        return fiblst
+    else:
+        fiblst=[0,1]
+        for i in range(2,N+1):
+            fiblst.append(fiblst[i-1]+fiblst[i-2])
+        print("All terms: ",fiblst)
+        #print("Last 10 terms: ",fiblst[-10:])
+        return fiblst
 
-def Fib(N,m):
+def Mult(N,m):
     fiblst=[0,1]
     multlst=[]
     for i in range(2,N+1):
@@ -17,9 +38,10 @@ def Fib(N,m):
         if x % m == 0:
             multlst.append(x)
     #print("List of #'s divisible by m: ", multlst)
-    print(len(multlst)," terms divisible by ",m)
+    print((len(multlst)/N)*100,"% of the first ",N,"are divisible by ",m)
     return multlst
 
-Fib(10,2)
+#Fib(16)
+Mult(1000,10)
                 
         
