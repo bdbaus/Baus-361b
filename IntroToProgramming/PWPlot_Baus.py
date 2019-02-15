ed00a8b5786df706b81596c8a61eaaeb58355315
Bryant Baus <bbaus@mail.umw.edu>
2019-02-15 12:08:41 -0500

    i.7 piecewise commit

--- IntroToProgramming/PWPlot_Baus.py
+++ IntroToProgramming/PWPlot_Baus.py
@@ -8,29 +8,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def func(x,N):
+    
+    x = np.linspace(-3, 3, N)
+    
+    
+    
     def f(x):
-        if(x < -2): 
+        if(x.any() < -2): 
             return -3*(x+2)**2+1
-        elif -2 <= x < -1:
+        elif -2 <= x.any() < -1:
             return 1
-        elif -1 <= x <= 1:
+        elif -1 <= x.any() <= 1:
             return (x-1)**3+3
         elif 1 < x < 2:
             return np.sin(np.pi*x)+3
         elif x >= 2:
-            return 3*(np.sqrt(x-2))+4
+            return 3*(np.sqrt(x.any()-2))+4
         return 0.0
 
-    f2 = np.vectorize(f)
-    
-    x = np.linspace(0., 10., N)
+ 
+    y = f(x)
 
-    y = f2(x)
-
-    plt.plot(x,y, '-')
-
-    plt.xlim([-3,3])
+    plt.plot(x,y, 'g-',linewidth=2.0)
 
     plt.show()
     
-func(x,10000)
+func(1,100)
